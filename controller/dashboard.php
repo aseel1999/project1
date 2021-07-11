2f06f2bdd5615452f1c7889c52b06409902f5cd5
iamAseel <aseelmaysoum@gmail.com>
2021-07-11 08:04:39 +0300

    added controllers and styles and index files

---
+++ controller/dashboard.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" type="text/css" href="../css/styleDashboard.css">
+    <title></title>
+</head>
+<body>
+    
+</body>
+</html>
